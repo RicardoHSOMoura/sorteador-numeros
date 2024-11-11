363b42dd9314f2b04dc98f99293f6c1f40c2f73b
Ricardo Moura <ricardohenriquetj@gmail.com>
2024-11-11 15:00:51 -0300

    escrevendo as funções

--- app.js
+++ app.js
@@ -3,27 +3,44 @@ function sortear() {
   let de  = parseInt(document.getElementById('de').value);
   let ate = parseInt(document.getElementById('ate').value);
 
-  let numeroAleatorio = []
+  let numerosAleatorios = []
   
-  numeroAleatorio = obterNumerosAleatorios(de,ate);
-  console.log(`numeroAleatorio: ${numeroAleatorio}`)
+  for (let i = 0; i < quantidade; i++) {
+    let numeros = obterNumerosAleatorios(de, ate)
+    while(numerosAleatorios.includes(numeros)) {
+      numeros = obterNumerosAleatorios(de, ate)
+    }
+    numerosAleatorios.push(numeros);
+
+    alterarBotaoReiniciar()
+  }
+
+  let resultado = document.getElementById('resultado')
+  resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${numerosAleatorios}</label>`
+  
+  console.log(`numerosAleatorios: ${numerosAleatorios}`)
 
 }
 
 function obterNumerosAleatorios(min, max) {
-  let contador = 0
-  while (contador < quantidade) {
-    let numAleat = Math.floor(Math.random() * (max - min + 1)) + min;
-    numeroAleatorio.push(numAleat)
-    contador++;
-  }
-  return numeroAleatorio;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function reiniciar() {
-  quantidade = '';
-  de = '';
-  ate = '';
-  numeroAleatorio = '';
+function alterarBotaoReiniciar() {
+  let botaoReiniciar = document.getElementById('btn-reiniciar')
+  if (botaoReiniciar.classList.contains('container__botao-desabilitado')) {
+    botaoReiniciar.classList.remove('container__botao-desabilitado');
+    botaoReiniciar.classList.add('container__botao')
+  } else {
+    botaoReiniciar.classList.remove('container__botao')
+    botaoReiniciar.classList.add('container__botao-desabilitado');
+  }
+}
 
+function reiniciar()  {
+  document.getElementById('quantidade').value = "";
+  document.getElementById('de').value = "";
+  document.getElementById('ate').value = "";
+  document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+  alterarBotaoReiniciar();
 }
